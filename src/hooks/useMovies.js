ef6d19e999047364b8a108ed7c906cb960a373b1
camiloUbuntu <53837383+calopez19@users.noreply.github.com>
2025-12-09 20:52:18 -0300

    Refactor useMovies hook to optimize search logic and prevent unnecessary API calls

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,15 +1,18 @@
 import withtResults from "../mocks/results.json";
 import withoutResults from "../mocks/no-results.json";
-import { useState } from "react";
-import {KEY} from '../constants.js'
+import { useState, useRef } from "react";
+import { KEY } from '../constants.js'
 import { getMovies } from "../services/getMovies.js";
 
 export function useMovies({ search }) {
   const [responseMovies, setResponseMovies] = useState([])
+  const previousMovieSearch = useRef(search)
 
-  const searchMovies = async({search}) => {
-    const newMovies = await getMovies({search})
+  const searchMovies = async ( ) => {
+    if (search === previousMovieSearch.current) return
+    previousMovieSearch.current = search
+    const newMovies = await getMovies({ search })
     setResponseMovies(newMovies)
   }
-  return { movies: responseMovies, searchMovies }  
+  return { movies: responseMovies, searchMovies }
 }
